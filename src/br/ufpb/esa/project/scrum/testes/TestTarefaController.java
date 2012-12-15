57f5512d19facca23f2ea7f48bb77a44c149232b
Vicente Neto <vicente.neto@dce.ufpb.br>
2012-12-14 23:08:45 -0200

    Commit Inicial dos Testes Unitarios

---
+++ src/br/ufpb/esa/project/scrum/testes/TestTarefaController.java
@@ -0,0 +1,16 @@
+package br.ufpb.esa.project.scrum.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTarefaController {
+	@Test
+	public void testCreateTarefa() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetTarefas() {
+		fail("Not yet implemented");
+	}
+}
